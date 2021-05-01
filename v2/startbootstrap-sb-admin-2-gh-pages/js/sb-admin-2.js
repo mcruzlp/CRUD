e74a4c3e1f68542623edfc75b5db2268b3a676e9
Cruz López Pérez <mcruzlp@gmail.com>
2021-05-01 18:09:12 +0200

    table added

--- v2/startbootstrap-sb-admin-2-gh-pages/js/sb-admin-2.js
+++ v2/startbootstrap-sb-admin-2-gh-pages/js/sb-admin-2.js
@@ -58,3 +58,60 @@
 $(document).ready(function () {
   $('#myTable').DataTable();
 });
+
+var editor; // use a global for the submit and return data rendering in the examples
+
+$(document).ready(function () {
+  editor = new $.fn.dataTable.Editor({
+    ajax: "../staff.php",
+    table: "#myTable",
+    fields: [{
+      label: "Nombre:",
+      name: "first_name"
+    }, {
+      label: "Apellidos:",
+      name: "last_name"
+    }, {
+      label: "Edad:",
+      name: "position"
+    }, {
+      label: "Teléfono:",
+      name: "office"
+    }, {
+      label: "Email:",
+      name: "extn"
+    }, {
+      label: "Fecha de alta:",
+      name: "start_date",
+      type: "datetime"
+    }, {
+      label: "Tarifa:",
+      name: "salary"
+    }
+    ]
+  });
+
+  $('#myTable').DataTable({
+    dom: "Bfrtip",
+    ajax: "../staff.php",
+    columns: [
+      {
+        data: null, render: function (data, type, row) {
+          // Combine the first and last names into a single table field
+          return data.first_name + ' ' + data.last_name;
+        }
+      },
+      { data: "position" },
+      { data: "office" },
+      { data: "extn" },
+      { data: "start_date" },
+      { data: "salary", render: $.fn.dataTable.render.number(',', '.', 0, '$') }
+    ],
+    select: true,
+    buttons: [
+      { extend: "create", editor: editor },
+      { extend: "edit", editor: editor },
+      { extend: "remove", editor: editor }
+    ]
+  });
+});
