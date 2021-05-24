b75a5002213684ecaca0c6a74eed8cb61702daf0
Cruz López Pérez <mcruzlp@gmail.com>
2021-05-24 19:25:40 +0200

    restart 2

--- others/staff.php
+++ others/staff.php
@@ -1,9 +1,12 @@
 <?php
+ 
 /*
  * Example PHP implementation used for the index.html example
  */
+ 
 // DataTables PHP library
 include( "../lib/DataTables.php" );
+ 
 // Alias Editor classes so they are easy to use
 use
     DataTables\Editor,
@@ -14,6 +17,7 @@ use
     DataTables\Editor\Upload,
     DataTables\Editor\Validate,
     DataTables\Editor\ValidateOptions;
+ 
 // Build our Editor instance and process the data coming from _POST
 Editor::inst( $db, 'datatables_demo' )
     ->fields(
